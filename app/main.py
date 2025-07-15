455c0d5b21a90c710c8c938a644285d7cede6a13
Tomer Shoob <tomer.shoob@MAC-TOMERSH.local>
2025-07-15 11:24:28 +0300

    new project

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+import boto3
+from dotenv import load_dotenv
+from boto3.dynamodb.conditions import Key
+# Load environment variables from .env
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/secret', methods=['GET'])
+def get_secret():
+    code_name = 'thedoctor'
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('devops-challenge')
+    try:
+        response = table.query(
+            KeyConditionExpression=Key(
+                'code_name').eq(code_name)
+        )
+        item = response.get('Item')
+        if not item or 'secret_code' not in item:
+            return jsonify({'error': 'Secret code not found'}), 404
+        return jsonify({'secret_code': {item['secret_code']}})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({
+        'status': 'healthy',
+        'container': '<LINK_TO_HUB>',
+        'project': 'github.com/omerxx/ecscale'
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
